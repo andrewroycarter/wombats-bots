4ea252341e261665a9df9a8356d911db2391b82c
Andrew Carter <andrew.carter@willowtreeapps.com>
2017-03-10 16:20:16 -0500

    fart bot now moves
    and farts

--- fart-bot.js
+++ fart-bot.js
@@ -1,24 +1,39 @@
-((state, timeLeftFn) => {
+;((state, timeLeftFn) => {
 
-    var fart = 1;
-    var walk = 2;
-    
-    var prev = state["saved-state"].prev || fart;
-    var move = prev === fart ? walk : fart;
-    
-    var command;
-    if (move === fart) {
-        command = { action: 'smoke', metadata: { direction: 'backward' } };
-    } else {
-        command = { action: 'move' };
+  const none = 0
+  const fart = 1
+  const walk = 2
+  const turn = 3
+
+  const previousState = state['saved-state'] || {}
+  const previousMove = previousState['previous-move'] || none
+  const lastTurnWasTurn = previousMove === turn
+  const shouldTurn = (Math.floor((Math.random() * 10) + 1) == 1) && !lastTurnWasTurn
+
+  var move
+  if (shouldTurn) {
+    move = turn
+  } else {
+    move = previousMove === fart ? walk : fart
+  }
+
+  var command
+  if (move === fart) {
+    command = { action: 'smoke', metadata: { direction: 'backward' } }
+  } else if (move === turn) {
+    const turnDirections = ['right', 'left', 'about-face']
+    const turnDirection = turnDirections[Math.floor(Math.random() * 3)]
+    command = { action: 'turn', metadata: { direction: turnDirection } }
+  } else if (move === walk) {
+    command = { action: 'move' }
+  } else {
+    command = { action: 'move' }
+  }
+
+  return {
+    command,
+    state: {
+      'previous-move': move
     }
-
-
-    return {
-        command,
-        state: {
-            prev: move
-        }
-    };
-});
-
+  }
+})
